5b01ed18851d1f878a43a17b4645eefb850e065e
steven91-ar <stevensinpra@gmail.com>
2023-06-19 12:25:30 +0200

    first commit

---
+++ src/main/java/com/doctor/hellodoctor/HellodoctorApplication.java
@@ -0,0 +1,13 @@
+package com.doctor.hellodoctor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellodoctorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellodoctorApplication.class, args);
+	}
+
+}
